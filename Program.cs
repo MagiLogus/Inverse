a2ea0135b9c571c12fca65d250638e46bf221de3
Paulo Henrique Gonçalves de Oliveira <paulo.oliveira.phgo@gmail.com>
2023-04-17 08:28:51 -0300

    atividade concluida

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// 7 - Faça um algoritmo para ler 15 números e armazenar em um vetor. Após a leitura total dos
+// 15 números, o algoritmo deve escrever esses 15 números lidos na ordem inversa da qual foi
+// declarado.
+
+int [] numeros = new int [15];
+
+for (int i = 0; i < 15; i++) 
+{
+    Console.WriteLine($"Digite o {i+1}º numero:");
+    numeros[i] = int.Parse(Console.ReadLine());
+}
+
+Console.WriteLine("Números na ordem inversa:");
+for (int i = 14; i > 0; i--) 
+{
+      Console.WriteLine(numeros[i]);
+}
